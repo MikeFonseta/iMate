6e4a844555d63d3a695a89d90fdfdfcbe9ec244f
mikefonseta <seta.mik@gmail.com>
2022-12-10 22:35:37 +0100

    Initial Commit

---
+++ iMate/iMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMateApp.swift
+//  iMate
+//
+//  Created by Mike Fonseta on 10/12/22.
+//
+
+import SwiftUI
+
+@main
+struct iMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
